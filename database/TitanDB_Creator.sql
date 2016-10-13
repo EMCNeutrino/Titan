d363df200d6b466ac598d901760ec2256df8ef41
Adrian Moreno <adrian@morenomartinez.com>
2016-10-13 16:58:08 +0200

    Updates database creation script

--- database/TitanDB_Creator.sql
+++ database/TitanDB_Creator.sql
@@ -23,8 +23,7 @@ USE titandb;
 GO
 
 /* Drop Tables */
-DROP TABLE IF EXISTS `item`;
-DROP TABLE IF EXISTS `penalty`;
+DROP TABLE IF EXISTS `equipment`;
 DROP TABLE IF EXISTS `hero`;
 
 /* Drop Functions */
@@ -33,65 +32,42 @@ DROP FUNCTION IF EXISTS `randomizer`;
 CREATE TABLE hero
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    hero_name TEXT,
-    token TEXT,
-    userpass TEXT,
-    energy INT,
-    twitter TEXT,
-    email TEXT,
-    title TEXT,
-    race TEXT,
-    is_admin BOOL,
-    level INT,
-    class TEXT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    class VARCHAR(255),
+    enabled BOOL DEFAULT 0,
+    token VARCHAR(255),
+    is_admin BOOL DEFAULT 0,
+    level INT DEFAULT 0,
     ttl INT,
-    i_online BOOL,
     xpos INT,
-    ypos INT
+    ypos INT,
+    UNIQUE (email)
 );
 
-CREATE TABLE item
+CREATE TABLE equipment
 (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    hero_id INT,
-    weapon INT,
-    tunic INT,
-    shield INT,
-    leggings INT,
-    ring INT,
-    gloves INT,
-    boots INT,
-    energy INT,
-    helm INT,
-    charm INT,
-    Amulet INT,
-    Total INT,
+    hero_id INT PRIMARY KEY NOT NULL,
+    ring INT DEFAULT 0,
+    amulet INT DEFAULT 0,
+    charm INT DEFAULT 0,
+    weapon INT DEFAULT 0,
+    helm INT DEFAULT 0,
+    tunic INT DEFAULT 0,
+    gloves INT DEFAULT 0,
+    shield INT DEFAULT 0,
+    leggings INT DEFAULT 0,
+    boots INT DEFAULT 0,
     CONSTRAINT item_hero_hero_id_fk FOREIGN KEY (hero_id) REFERENCES hero (id)
 );
 
-
-CREATE TABLE penalty
-(
-    penalty_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    hero_id INT,
-    logout INT,
-    quit INT,
-    message INT,
-    quest INT,
-    CONSTRAINT penalty_hero_hero_id_fk FOREIGN KEY (hero_id) REFERENCES hero (id)
-);
-
-
 /* Add comments to each Table */
-ALTER TABLE item COMMENT = 'Holds the items owned by the hero';
-ALTER TABLE penalty COMMENT = 'Penalties table stores the penalties accumulated by a hero';
 ALTER TABLE hero COMMENT = 'Contains the Hero information';
+ALTER TABLE equipment COMMENT = 'Holds the items owned by the hero';
 
 
 /* Functions */
 
-
-/* Random Function */
 /* http://stackoverflow.com/questions/14798640/creating-a-random-number-using-mysql */
 CREATE FUNCTION randomizer(
     pmin INTEGER,
