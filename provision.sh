05b99c06855eb91e903404798dfa786da6c20515
Adrian Moreno <adrian@morenomartinez.com>
2016-10-20 12:21:18 +0200

    Fixes vagrant provision script

--- provision.sh
+++ provision.sh
@@ -16,13 +16,13 @@ usermod -aG docker ubuntu
 echo -e "\n--- Install MySQL specific packages and settings ---\n"
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_PASSWD"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_PASSWD"
-apt-get -y install mysql-server
+apt-get -y install mysql-server libmysqlclient-dev
 
 echo -e "\n--- Install Redis server ---\n"
 apt-get install -y redis-server
 
 echo -e "\n--- Install Python packages for Registration website ---\n"
-pip3 install -r /home/ubuntu/src/web/requirements.txt
+pip3 install -r /home/ubuntu/src/hero-web/requirements.txt
 
 echo -e "\n--- Install Golang 1.7 ---\n"
 cd /home/ubuntu
@@ -32,7 +32,7 @@ chown -R root:root ./go
 rm -rf /usr/local/go
 mv go /usr/local
 mkdir -p ./work/github.com/VxRackNeutrino/Hero/
-ln -s /home/ubuntu/src/game-engine ./work/github.com/VxRackNeutrino/Hero/game-engine
+ln -s /home/ubuntu/src/hero-engine ./work/github.com/VxRackNeutrino/Hero/hero-engine
 cat <<EOT >> ./.profile
 export GOPATH=/home/ubuntu/work
 export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
