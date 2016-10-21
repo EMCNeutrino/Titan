6e400f77acae85b2da5f808a6d2ea5d5db922ec2
Lida He <lida.he@dell.com>
2016-10-21 02:19:33 +0000

    wrapper script to register hero

--- scripts/register-hero.sh
+++ scripts/register-hero.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# A place holder for now
-cat >> /tmp/hero-env-from-user << EOF
+cat >> /home/ubuntu/hero-env-from-user << EOF
 export HERO_FIRST_NAME=${HERO_FIRST_NAME}
 export HERO_LAST_NAME=${HERO_LAST_NAME}
 export HERO_NAME=${HERO_NAME}
 export HERO_EMAIL=${HERO_EMAIL}
-export HERO_TWITTER${HERO_TWITTER}
+export HERO_TWITTER=${HERO_TWITTER}
+export HERO_TOKEN=${HERO_TOKEN}
 EOF
 
-python /home/ubuntu/Hero/clients/python/Hero-Bot-Client.py
+python /home/ubuntu/Hero/clients/python/Hero-Bot-Client.py > register-hero.log 2>&1 &
