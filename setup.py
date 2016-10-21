2080466745fced1b920204d1157072e4ee0b43f9
Patrick Butler Monterde <pbutlerm@gmail.com>
2016-10-20 18:19:30 -0700

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Hero-Client',
+    version='1.0',
+    packages=[''],
+    url='http://www.vxrackneutrinoheros.com',
+    license='MIT',
+    author='DellEMC',
+    author_email='',
+    description='Hero Client'
+)
