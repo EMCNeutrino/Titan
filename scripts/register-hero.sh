c41f727ea8e403777c7ac6b85cc22102bd61fb9d
Lida He <lida.he@dell.com>
2016-10-21 00:17:42 +0000

    place holder for the script that user will execute

---
+++ scripts/register-hero.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# A place holder for now
+cat >> /tmp/hero-env-from-user << EOF
+export HERO_FIRST_NAME=${HERO_FIRST_NAME}
+export HERO_LAST_NAME=${HERO_LAST_NAME}
+export HERO_NAME=${HERO_NAME}
+export HERO_EMAIL=${HERO_EMAIL}
+export HERO_TWITTER${HERO_TWITTER}
+EOF
