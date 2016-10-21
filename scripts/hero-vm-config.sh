a02e884e8aed4b9a18f762aea7b71c916376b15a
Lida He <lida.he@dell.com>
2016-10-21 02:28:07 +0000

    added more env variable to user script

--- scripts/hero-vm-config.sh
+++ scripts/hero-vm-config.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-export GAME_CONTROLLER_IP=10.246.154.49
-export HERO_NAME=`hostname`
-export HERO_TITLE="Neutrino Guru"
+export HERO_FIRST_NAME=Gandalf
+export HERO_LAST_NAME=TheGray
+export HERO_NAME=Magician
+export HERO_EMAIL="gandalf_thegray@example.com"
+# authentication token you received after registration
+export HERO_TOKEN=1234
+# optional Twitter account
+export HERO_TWITTER="Gandalf"
 
-# should preinstall python and git to speed up boot process
-# install python setup tools
-apt-get install -y python-setuptools python-dev build-essential
-
-# install git
-apt-get install -y git
-
-git config --global http.sslVerify false
-
-cd /opt
-# clone Hero project as ubuntu user
-git clone https://github.com/VxRackNeutrino/Hero
-
-python /opt/Hero/scripts/hero-register-test.py
+# ----------------------------------------
+export HERO_ENGINE_IP=10.246.152.15
+/home/ubuntu/Hero/script/register-hero.sh
